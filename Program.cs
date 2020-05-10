cb44a6a5dda128d074d8468d282fb7aa432a4e39
swanable <swanable0615@endalgo.com>
2020-05-10 14:25:21 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+
+namespace QueueApp
+{
+    class Program
+    {
+        private const string connectionString = "DefaultEndpointsProtocol=https;AccountName=stisystemstorage;AccountKey=E5LoVqDOXLRDDke6OOXMsEZ8xQl07cMHdS7ikT9vBwejN/zcXaePR7+CijKMSjcCd05X/yRiNTccxmonKsu/rA==;EndpointSuffix=core.windows.net";
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("blog-queue");
+
+            //if (args.Length > 0)
+            if (false)
+            {
+                //string value = String.Join(" ", args);
+                string value = "test";
+                await SendMessageAsync(queue, value);
+                Console.WriteLine($"Sent: {value}");
+            }
+            else
+            {
+                string value = await ReceiveMessageAsync(queue);
+                Console.WriteLine($"Received: {value}");
+            }
+
+            Console.Write("Press Enter...");
+            Console.ReadLine();
+        }
+
+        static async Task SendMessageAsync(CloudQueue theQueue, string newMessage)
+        {
+            bool createdQueue = await theQueue.CreateIfNotExistsAsync();
+
+            if (createdQueue)
+            {
+                Console.WriteLine("The queue was created.");
+            }
+
+            CloudQueueMessage message = new CloudQueueMessage(newMessage);
+            await theQueue.AddMessageAsync(message);
+        }
+
+        static async Task<string> ReceiveMessageAsync(CloudQueue theQueue)
+        {
+            bool exists = await theQueue.ExistsAsync();
+
+            if (exists)
+            {
+                CloudQueueMessage retrievedMessage = await theQueue.GetMessageAsync();
+
+                if (retrievedMessage != null)
+                {
+                    string theMessage = retrievedMessage.AsString;
+                    await theQueue.DeleteMessageAsync(retrievedMessage);
+                    return theMessage;
+                }
+                else
+                {
+                    Console.Write("The queue is empty. Attempt to delete it? (Y/N) ");
+                    string response = Console.ReadLine();
+
+                    if (response == "Y" || response == "y")
+                    {
+                        await theQueue.DeleteIfExistsAsync();
+                        return "The queue was deleted.";
+                    }
+                    else
+                    {
+                        return "The queue was not deleted.";
+                    }
+                }
+            }
+            else
+            {
+                return "The queue does not exist. Add a message to the command line to create the queue and store the message.";
+            }
+        }
+    }
+}
